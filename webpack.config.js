3da4480171fcfdac9ace5aa3cfa04ca4db5fd1fe
Lily <1368025+Lily418@users.noreply.github.com>
2024-12-12 18:35:21 +0000

    Remove unused css deps

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,7 @@
 const path = require('path');
 const defaultConfig = require('@wordpress/scripts/config/webpack.config');
 const WooCommerceDependencyExtractionWebpackPlugin = require('@woocommerce/dependency-extraction-webpack-plugin');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
-// Remove SASS rule from the default config so we can define our own.
-const defaultRules = defaultConfig.module.rules.filter((rule) => {
-	return String(rule.test) !== String(/\.(sc|sa)ss$/);
-});
 
 module.exports = {
 	...defaultConfig,
@@ -14,14 +9,6 @@ module.exports = {
 		...defaultConfig.module,
 		rules: [
 			...defaultRules,
-			{
-				test: /\.(sc|sa)ss$/,
-				exclude: /node_modules/,
-				use: [
-					MiniCssExtractPlugin.loader,
-					{ loader: 'css-loader', options: { importLoaders: 1 } },
-				],
-			},
 		],
 	},
 	plugins: [
@@ -30,8 +17,5 @@ module.exports = {
 				plugin.constructor.name !== 'DependencyExtractionWebpackPlugin'
 		),
 		new WooCommerceDependencyExtractionWebpackPlugin(),
-		new MiniCssExtractPlugin({
-			filename: `[name].css`,
-		}),
 	],
 };
