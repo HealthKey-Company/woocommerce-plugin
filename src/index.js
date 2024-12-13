ce1aba127efc4cd1f3b3e88867c59cbad380bbc8
Lily <1368025+Lily418@users.noreply.github.com>
2024-12-13 10:27:02 +0000

    Stripe wrap the React components in a fragment, this avoids console errors about react components receiving the wrong props.

--- src/index.js
+++ src/index.js
@@ -3,27 +3,32 @@ const { registerPaymentMethod } = window.wc.wcBlocksRegistry;
 const data = window.wc.wcSettings.getSetting("healthkey_payment_data");
 
 const Label = (
-    <span style={{ width: "100%" }}>
-        <div
-            style={{
-                display: "flex",
-                justifyContent: "space-between",
-            }}
-        >
-            <span>Pay with HealthKey</span> <img src={data.icon} />
-        </div>
-    </span>
+    <>
+
+        <span style={{ width: "100%" }}>
+            <div
+                style={{
+                    display: "flex",
+                    justifyContent: "space-between",
+                }}
+            >
+                <span>Pay with HealthKey</span> <img src={data.icon} />
+            </div>
+        </span>
+    </>
 );
 
 const Content = (
-    <div style={{
-        display: "flex",
-        flexDirection: "column",
-        gap: "0.25rem"
-    }}>
-        <span className="content" style={{ fontSize: "14px", fontWeight: "400" }}>You’ll be asked to sign in or create a free account to authorise the payment</span>
-        <span className="content" style={{ fontSize: "14px", fontWeight: "700" }}>Get discounts on vetted health care with HealthKey</span>
-    </div >
+    <>
+        <div style={{
+            display: "flex",
+            flexDirection: "column",
+            gap: "0.25rem"
+        }}>
+            <span className="content" style={{ fontSize: "14px", fontWeight: "400" }}>You’ll be asked to sign in or create a free account to authorise the payment</span>
+            <span className="content" style={{ fontSize: "14px", fontWeight: "700" }}>Get discounts on vetted health care with HealthKey</span>
+        </div>
+    </>
 );
 
 const paymentRequestPaymentMethod = {
@@ -35,7 +40,9 @@ const paymentRequestPaymentMethod = {
     description: "Pay via Healthkey",
     gatewayId: "healthkey_payment",
     content: Content,
-    edit: <h2>Pay with Healthkey</h2>,
+    edit: <>
+        <h2>Pay with Healthkey</h2>
+    </>,
     canMakePayment: () => true,
     paymentMethodId: "healthkey_payment",
     supports: {
