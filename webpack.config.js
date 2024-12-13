f2ef986d4b8c7b97000a8e487459df1d1802ef3b
Lily <1368025+Lily418@users.noreply.github.com>
2024-12-13 14:43:59 +0000

    Correct webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,8 @@
-const path = require('path');
 const defaultConfig = require('@wordpress/scripts/config/webpack.config');
 const WooCommerceDependencyExtractionWebpackPlugin = require('@woocommerce/dependency-extraction-webpack-plugin');
 
-
 module.exports = {
 	...defaultConfig,
-	module: {
-		...defaultConfig.module,
-		rules: [
-			...defaultRules,
-		],
-	},
 	plugins: [
 		...defaultConfig.plugins.filter(
 			(plugin) =>
